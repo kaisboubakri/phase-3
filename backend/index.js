ca0e659849c25fa0db9790a27425bb0395727255
kaisboubakri <kais.boubakri1998@gmail.com>
2024-03-03 20:05:05 +0100

    first commit

---
+++ backend/index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const { default: mongoose } = require("mongoose");
+const app = express();
+const cors = require("cors");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+dotenv.config();
+
+app.use(express.json());
+
+const userRoutes = require("./Routes/userRoutes");
+const chatRoutes = require("./Routes/chatRoutes");
+
+const connectDb = async () => {
+  try {
+    const connect = await mongoose.connect(process.env.MONGO_URI);
+    console.log("Server is Connected to Database");
+  } catch (err) {
+    console.log("Server is NOT connected to Database", err.message);
+  }
+};
+connectDb();
+
+app.get("/", (req, res) => {
+  res.send("API is running123");
+});
+
+app.use("/user", userRoutes);
+app.use("/chat", chatRoutes);
+
+// Error Handling middlewares
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server is Running...`, PORT));
